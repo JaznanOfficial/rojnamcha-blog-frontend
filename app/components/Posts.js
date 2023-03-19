6939b844219f665004e4afd81dd8883bf9ea7153
JaznanOfficial <jaznanofficial@gmail.com>
2023-03-19 09:24:16 +0600

    pagination done

--- app/components/Posts.js
+++ app/components/Posts.js
@@ -1,8 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import Post from "./Post";
 import { Crete_Round } from "next/font/google";
+import ReactPaginate from "react-paginate";
 
 const crete = Crete_Round({
     subsets: ["latin"],
@@ -11,6 +12,18 @@ const crete = Crete_Round({
 });
 
 const Posts = () => {
+    const allPosts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+    // pagination
+    const [pageNumber, setPageNumber] = useState(0);
+    const postsPerPage = 9;
+    const skip = postsPerPage * pageNumber;
+    const displayPosts = allPosts.slice(skip, skip + postsPerPage);
+    const totalPage = Math.ceil(allPosts.length / postsPerPage);
+    // console.log(totalPage);
+    const pageChange = ({ selected }) => {
+        setPageNumber(selected);
+    };
     return (
         <div className="mt-5">
             <h1
@@ -26,10 +39,25 @@ const Posts = () => {
                     ))}
                 </Row>
             </Container>
-            <div className="my-5 text-center">
-                <button type="button" class="btn btn-danger">
-                    See All Posts
-                </button>
+
+            <div className="py-5 my-5 text-center w-75 mx-auto">
+                <ReactPaginate
+                    previousLabel={"<< Prev"}
+                    nextLabel={"Next >>"}
+                    pageCount={totalPage}
+                    onPageChange={pageChange}
+                    breakLabel={"..."}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={3}
+                    containerClassName={
+                        "pagination d-flex align-items-center justify-content-center"
+                    }
+                    activeLinkClassName={"btn btn-active text-white bg-danger border-danger"}
+                    pageLinkClassName={"btn text-gray hover:bg-red-500 hover:border-red-500 mx-1"}
+                    previousLinkClassName={"btn text-danger prev-next-link "}
+                    nextLinkClassName={"btn text-danger prev-next-link"}
+                    breakLinkClassName={"btn text-danger prev-next-link"}
+                />
             </div>
         </div>
     );
