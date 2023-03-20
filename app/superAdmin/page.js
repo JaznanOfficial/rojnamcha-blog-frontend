8509ba41a7c37075ae545694b401d8ee3600230d
JaznanOfficial <jaznanofficial@gmail.com>
2023-03-20 10:37:53 +0600

    jodit pro added

--- app/superAdmin/page.js
+++ app/superAdmin/page.js
@@ -2,35 +2,49 @@
 import React, { useRef, useState } from "react";
 import Posts from "../components/Posts";
 import { Button, Container } from "react-bootstrap";
-import JoditEditor from "jodit-react";
+// import JoditEditor from "jodit-react";
+import dynamic from "next/dynamic";
+const JoditEditor = dynamic(() => import("jodit-react"), { ssr: false });
 
 const superAdmin = ({ placeholder }) => {
-    const editor = useRef(null);
-    //   const [content, setContent] = useState("");
+    // const editor = useRef(null);
+    const [content, setContent] = useState("");
 
     const config = {
         readonly: false,
+        uploader: {
+            url: "https://xdsoft.net/jodit/finder/?action=fileUpload",
+        },
+        filebrowser: {
+            ajax: {
+                url: "https://xdsoft.net/jodit/finder/",
+            },
+            height: 580,
+        },
         height: 500,
         placeholder: placeholder || "Start typings...",
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const content = {
-            description: editor.current.value,
-        };
+        // const content = {
+        //     description: editor.current.value,
+        // };
         console.log(content);
     };
 
     return (
         <Container>
             <JoditEditor
-                ref={editor}
+                // ref={editor}
                 // value={content}
+
                 config={config}
                 tabIndex={1} // tabIndex of textarea
                 // onBlur={(newContent) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-                onChange={(newContent) => {}}
+                onChange={(newContent) => {
+                    setContent(newContent);
+                }}
             />
             <Button variant="outline-danger" className="m-1" onClick={handleSubmit}>
                 <strong>Post this Blog</strong>
