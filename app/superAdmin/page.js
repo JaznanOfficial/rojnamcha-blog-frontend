5eb7dc960d656034110a4a24a7823f7e7bfcf827
JaznanOfficial <jaznanofficial@gmail.com>
2023-03-20 10:14:29 +0600

    jodit react added

---
+++ app/superAdmin/page.js
@@ -0,0 +1,43 @@
+"use client";
+import React, { useRef, useState } from "react";
+import Posts from "../components/Posts";
+import { Button, Container } from "react-bootstrap";
+import JoditEditor from "jodit-react";
+
+const superAdmin = ({ placeholder }) => {
+    const editor = useRef(null);
+    //   const [content, setContent] = useState("");
+
+    const config = {
+        readonly: false,
+        height: 500,
+        placeholder: placeholder || "Start typings...",
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const content = {
+            description: editor.current.value,
+        };
+        console.log(content);
+    };
+
+    return (
+        <Container>
+            <JoditEditor
+                ref={editor}
+                // value={content}
+                config={config}
+                tabIndex={1} // tabIndex of textarea
+                // onBlur={(newContent) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+                onChange={(newContent) => {}}
+            />
+            <Button variant="outline-danger" className="m-1" onClick={handleSubmit}>
+                <strong>Post this Blog</strong>
+            </Button>
+            <Posts />
+        </Container>
+    );
+};
+
+export default superAdmin;
